ed3b0bdbdd31f0818be70afd4ee56fac4921a9d5
null <404788@student.fontys.nl>
2021-10-11 17:22:23 +0200

    Audit 'fixes'
    Remove SafeMath (sol 0.8 deprecation)
    Add function comments
    Make non-internal method external to save gas
    
    Non-audit fix: prevent dev wallet from chaning fee type to prevent DoS

--- token.sol
+++ token.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.0;
 
 // @openzeppelin libs
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+// NOTE: as of 0.8.0 SafeMath is not necessary anymore
 
-contract OBToken is ERC20, ERC20Burnable, Ownable {
-    using SafeMath for uint256;
-
+contract OBToken is ERC20Burnable, Ownable {
     // Vesting in 6-months interval (uint identifies the schedule)
     // The schedules define the amount an address is NOT allow to use (the vested amount)
     // This allows users to buy more and use that amount freely, but not the amount obtained through a partner program
@@ -30,41 +27,72 @@ contract OBToken is ERC20, ERC20Burnable, Ownable {
         _fee_divider = 200; // 0.5%
     }
 
-    function setDevWallet(address addr) public onlyOwner {
-        _dev_wallet = addr;
+    /** @dev Sets Development wallet address
+      * @param addr destination address
+      */
+    function setDevWallet(address addr) external onlyOwner {
         setFeelessAddress(addr, 1);
+        _dev_wallet = addr;
     }
 
-    function setFee(uint256 fee_divider) public onlyOwner {
+    /** @dev Sets protocol fee
+      * @param fee_divider The fee divider (1/fee, where 0 < fee < 1)
+      */
+    function setFee(uint256 fee_divider) external onlyOwner {
         // Restrict fee range to prevent abuse from our side
         require(fee_divider >= 20, "Fee needs to be lower or equal to 5%");
         _fee_divider = fee_divider;
     }
     
-    function currentFeeDivider() public view returns (uint256) {
+    /** @dev Get current protocol fee
+      * @return fee divider (1/fee)
+      */
+    function currentFeeDivider() external view returns (uint256) {
         return _fee_divider;
     }
 
+    /** @dev Set if an address should pay protocol fees (some addresses need to be whitelisted)
+      * @param addr Address to configure
+      * @param isFeeless Fee structure the addr gets (0 = protocol fee, 1 = no fees)
+      */
     function setFeelessAddress(address addr, uint isFeeless) public onlyOwner {
+        require(addr != _dev_wallet, "Fee setting of dev wallet can't be changed as it needs to be 1");
     	_feelessmembers[addr] = isFeeless;
     }
 
-    function addVestingScheme(uint256[] calldata vestingamounts) public onlyOwner returns (uint) {
+    /** @dev Adds a vesting scheme to the contract
+      * @param vestingamounts The amount the needs to be locked at each point in time (in `_vestinginterval` increments from deployment)
+      * @return Vesting identifier
+      */
+    function addVestingScheme(uint256[] calldata vestingamounts) external onlyOwner returns (uint) {
         _maxvestingid += 1;
         _vestingschedules[_maxvestingid] = vestingamounts;
         return _maxvestingid;
     }
 
-    function addVestingAddress(address addr, uint vestingid) public onlyOwner {
+    /** @dev Adds an address to a vesting scheme (one per address)
+      * @param addr Address to configure
+      * @param vestingid Vesting id to add address to
+      */
+    function addVestingAddress(address addr, uint vestingid) external onlyOwner {
     	require(_vestingaddresses[addr] == 0, "Cannot change vesting schedule after it's set");
         _vestingaddresses[addr] = vestingid;
     }
 
-    function vestingRequirements(address addr) public view returns (uint256[] memory) {
+    /** @dev Get vesting requirements for a particular address
+      * @param addr Address to get requirements for
+      * @return the locked token requirements for `_vestinginterval` increments since contract creation
+      */ 
+    function vestingRequirements(address addr) external view returns (uint256[] memory) {
     	uint vestid = _vestingaddresses[addr];
         return _vestingschedules[vestid];
     }
 
+    /** @dev Transfers tokens (override for vesting and protocol fee)
+      * @param sender The origin of the tokens
+      * @param recipient The destination of the tokens
+      * @param amount The amount to transfer
+      */
     function _transfer(address sender, address recipient, uint256 amount) internal override {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
